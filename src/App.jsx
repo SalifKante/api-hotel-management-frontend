aaba00a144a23970cd97d268da96e4d87c99585c
Salif Kanté <kantesalif1920@gmail.com>
2024-03-09 14:00:17 +0000

    finished with adding room view

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddRoom from "./components/room/AddRom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <AddRoom />
+    </>
+  );
+}
+
+export default App;
